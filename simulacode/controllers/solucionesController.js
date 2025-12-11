6a27a151de32a5ef7a01da6fe00c27fd23831780
MarkTech32 <marktech370@gmail.com>
2025-12-11 03:43:07 -0600

    PRs (Aprobacion y Rechazos)
    Ahora se pueden aprobar o rechazar PRs dejando un comentario del porque

--- simulacode/controllers/solucionesController.js
+++ simulacode/controllers/solucionesController.js
@@ -1,4 +1,6 @@
 const Solucion = require('../models/Solucion');
+const Reto = require('../models/Reto');
+const octokit = require('../config/github');
 
 exports.registrarSolucion = async (req, res) => {
   const solucion = new Solucion(req.body);
@@ -14,4 +16,49 @@ exports.listarSoluciones = async (req, res) => {
 exports.listarSolucionesPorReto = async (req, res) => {
   const soluciones = await Solucion.find({ retoId: req.params.retoId });
   res.json(soluciones);
+};
+
+exports.aprobarPR = async (req, res) => {
+  const reto = await Reto.findById(req.params.retoId);
+  const [owner, repo] = reto.repositorio.split('/');
+  
+  await octokit.issues.createComment({
+    owner: owner,
+    repo: repo,
+    issue_number: req.body.prNumber,
+    body: req.body.comentario
+  });
+  
+  await octokit.pulls.merge({
+    owner: owner,
+    repo: repo,
+    pull_number: req.body.prNumber
+  });
+  
+  await Solucion.findByIdAndUpdate(req.params.solucionId, { estado: 'aprobada' });
+  
+  res.json({ message: 'PR aprobado y merged' });
+};
+
+exports.rechazarPR = async (req, res) => {
+  const reto = await Reto.findById(req.params.retoId);
+  const [owner, repo] = reto.repositorio.split('/');
+  
+  await octokit.issues.createComment({
+    owner: owner,
+    repo: repo,
+    issue_number: req.body.prNumber,
+    body: req.body.comentario
+  });
+  
+  await octokit.pulls.update({
+    owner: owner,
+    repo: repo,
+    pull_number: req.body.prNumber,
+    state: 'closed'
+  });
+  
+  await Solucion.findByIdAndUpdate(req.params.solucionId, { estado: 'rechazada' });
+  
+  res.json({ message: 'PR rechazado y cerrado' });
 };
--- simulacode/routes/soluciones.js
+++ simulacode/routes/soluciones.js
@@ -5,5 +5,7 @@ const solucionesController = require('../controllers/solucionesController');
 router.post('/soluciones', solucionesController.registrarSolucion);
 router.get('/soluciones', solucionesController.listarSoluciones);
 router.get('/retos/:retoId/soluciones', solucionesController.listarSolucionesPorReto);
+router.post('/retos/:retoId/soluciones/:solucionId/aprobar', solucionesController.aprobarPR);
+router.post('/retos/:retoId/soluciones/:solucionId/rechazar', solucionesController.rechazarPR);
 
 module.exports = router;
